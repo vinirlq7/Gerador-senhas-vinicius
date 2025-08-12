3146b08fd21d2e491da6a0a1ec8e8c32aec75ece
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2025-08-12 08:50:32 -0300

    12/08

---
+++ main.js
@@ -0,0 +1,15 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha; 
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if(tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha - 1;
+    }
+    numeroSenha.textContent=tamanhoSenha;
+}
