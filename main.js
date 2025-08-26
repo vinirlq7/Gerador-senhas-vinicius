db6f296fbe7b67dd990430f04c2069b9ef45aa04
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2025-08-26 08:46:47 -0300

    26/08

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function diminuiTamanho(){
         tamanhoSenha = tamanhoSenha - 1;
     }
     numeroSenha.textContent=tamanhoSenha;
+    geraSenha();
 }
 
 function aumentaTamanho(){
@@ -19,4 +20,23 @@ function aumentaTamanho(){
         tamanhoSenha = tamanhoSenha + 1;
     }
     numeroSenha.textContent=tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '#@?!*/&%$';
+geraSenha();
+
+function geraSenha(){
+    let senha = '';
+    for (let i=0; i<tamanhoSenha; i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
 }
